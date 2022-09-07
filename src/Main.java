076c625a0149fe1067403b6dbd7f46e32ef17ce3
Whitedragon1987 <vanoostenjohan@hotmail.com>
2022-09-07 15:38:19 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hallo, wereld");
+        System.out.println("hallo, welkom bij de eerste git workshop van jullie opleiding tot developers ");
     }
 }
